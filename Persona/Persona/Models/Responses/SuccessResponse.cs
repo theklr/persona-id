ec44e916d89245d6250a89f38b1e57725a6d5493
Gema Ramila <gramila65@gmail.com>
2015-10-11 02:18:04 -0400

    logIn

---
+++ Persona/Persona/Models/Responses/SuccessResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Persona.Models.Responses
+{
+    public class SuccessResponse : BaseResponse
+    {
+        public SuccessResponse()
+        {
+
+            this.IsSuccessFull = true;
+
+        }
+    }
+}
