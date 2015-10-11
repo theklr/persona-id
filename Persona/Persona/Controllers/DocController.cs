59f01962d167edb269176dd3df3c34da72f7397d
Kevin Reevers <kreevers@gmail.com>
2015-10-11 08:24:11 -0400

    fixes to routes

---
+++ Persona/Persona/Controllers/DocController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Persona.Controllers
+{
+    public class DocController : Controller
+    {
+        // GET: Doc
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Portfolio()
+        {
+            return View();
+        }
+
+        public ActionResult Verfication()
+        {
+            return View();
+        }
+        public ActionResult Confirmation()
+        {
+            return View();
+        }
+    }
+}
